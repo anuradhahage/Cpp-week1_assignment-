3be6a37b61ee55931c0460f31ad9d95bfc9d89e1
Anuradha Hage <vhage656@gmail.com>
2026-09-08 07:54:28 +0530

    Add program to calculate sum and average of array

---
+++ array_sum_average.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    float sum = 0, average;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    float arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    average = sum / n;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << average << endl;
+
+    return 0;
+}
